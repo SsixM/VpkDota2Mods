b6cd2317545fddb3c000f5b7f20ad7fd1eea7137
Mr. Cub <coinmcmc@gmail.com>
2025-12-30 23:02:18 +0300

    1

--- vpk/loader_en.bat
+++ vpk/loader_en.bat
@@ -3,26 +3,20 @@ setlocal enabledelayedexpansion
 title VPK CORE - MOD INSTALLER
 
 echo ======================================================
-echo             VPK CORE - MOD INSTALLER
+echo             VPK CORE - MOD INSTALLER (PRO)
 echo ======================================================
 echo.
 
-:: 1. ПРОВЕРКА ФАЙЛОВ
 if not exist "%~dp0data\*.vpk" (
-    echo [!] ERROR: Mod files not found.
-    echo.
-    echo YOU MUST EXTRACT THE "MODS" FOLDER FROM THE ZIP!
-    echo Do not run the script inside the archive.
+    echo [!] ERROR: Mod files not found in 'data' folder.
     echo.
     pause
     exit /b
 )
 
 set "D_PATH="
-:: Ищем путь через реестр
 for /f "tokens=2*" %%A in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 570" /v "InstallLocation" 2^>nul') do set "D_PATH=%%B"
 
-:: Если реестр пуст - ищем по дискам
 if "%D_PATH%"=="" (
     for %%D in (C D E F G H) do (
         if exist "%%D:\SteamLibrary\steamapps\common\dota 2 beta\game\dota" set "D_PATH=%%D:\SteamLibrary\steamapps\common\dota 2 beta"
@@ -30,58 +24,79 @@ if "%D_PATH%"=="" (
     )
 )
 
-:: Если совсем не нашли - ручной ввод
 if "%D_PATH%"=="" (
     echo [!] Dota 2 not found automatically.
     set /p D_PATH="Enter path to 'dota 2 beta' folder: "
 )
 set "D_PATH=%D_PATH:"=%"
 
-if not exist "%D_PATH%\game\dota" (
+set "G_DIR=%D_PATH%\game"
+set "MOD_DIR=%G_DIR%\VPKCORE"
+set "GI_FILE=%G_DIR%\dota\gameinfo_branchspecific.gi"
+
+if not exist "%G_DIR%\dota" (
     echo [!] ERROR: Invalid path. 'game\dota' not found.
     pause
     exit /b
 )
 
-set "G_DIR=%D_PATH%\game"
-cls
-echo Target: %D_PATH%
-echo.
-echo Select your language:
-set i=0
+echo [+] Target: %D_PATH%
 
-:: RU Всегда первый
-if exist "%G_DIR%\dota_russian" (
-    set /a i+=1
-    set "f[!i!]=dota_russian"
-    echo [!i!] dota_russian
+if not exist "%MOD_DIR%" (
+    echo [+] Creating VPKCORE directory...
+    mkdir "%MOD_DIR%"
 )
 
-:: Остальные кроме RU и аддонов
-for /d %%D in ("%G_DIR%\dota_*") do (
-    set "n=%%~nxD"
-    if /i not "!n!"=="dota_russian" if /i not "!n!"=="dota_addons" (
-        set /a i+=1
-        set "f[!i!]=!n!"
-        echo [!i!] !n!
-    )
-)
+echo [+] Copying mods...
+del /q "%MOD_DIR%\*.vpk" >nul 2>&1
+copy /y "%~dp0data\*.vpk" "%MOD_DIR%\" >nul
 
+echo [+] Patching gameinfo_branchspecific.gi...
+(
+echo "GameInfo"
+echo {
+echo     game         "Dota 2"
+echo     title        "Dota 2"
 echo.
-set /p c="Choice: "
-if not defined f[%c%] ( echo Invalid. & pause & exit /b )
-
-set "T=!f[%c%]!"
-set "L_P=!T:dota_=!"
-
+echo     FileSystem
+echo     {
+echo         SteamAppId				570
+echo         BreakpadAppId			373300
+echo         BreakpadAppId_Tools		375360
 echo.
-echo [+] Installing to !T!...
-del /q "%G_DIR%\!T!\pak*_dir.vpk" >nul 2>&1
-copy /y "%~dp0data\*.vpk" "%G_DIR%\!T!\" >nul
+echo         SearchPaths
+echo         {
+echo             Game				VPKCORE
+echo             Mod					VPKCORE
+echo             Game_Language		dota_*LANGUAGE*
+echo             Game_LowViolence	dota_lv
+echo.
+echo             Game				dota
+echo             Game				core
+echo.
+echo             Mod					dota
+echo.
+echo             Write				dota
+echo.
+echo             AddonRoot_Language	dota_*LANGUAGE*_addons
+echo             AddonRoot			dota_addons
+echo.
+echo             PublicContent		dota_core
+echo             PublicContent		core
+echo         }
+echo.
+echo         "UserSettingsPathID"	"USRLOCAL"
+echo         "LegacyUserSettingsPathID"	"MOD"
+echo.
+echo         AddonsChangeDefaultWritePath 0
+echo     }
+echo }
+) > "%GI_FILE%"
 
 echo.
 echo ======================================================
 echo SUCCESS!
-echo Add to Steam launch options: -language !L_P!
+echo Mods installed to VPKCORE.
+echo GameInfo updated. No launch options required!
 echo ======================================================
 pause
